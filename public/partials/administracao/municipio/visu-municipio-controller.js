a2b3c78c3638e399a5bda6e2464765958cb21999
Eduardo S S Rodrigues <eduardo070294@gmail.com>
2019-03-25 12:20:21 -0300

    filtro municipio

--- public/partials/administracao/municipio/visu-municipio-controller.js
+++ public/partials/administracao/municipio/visu-municipio-controller.js
@@ -22,15 +22,20 @@ angular.module('cidadesdigitais').controller('visuMunicipiosController', functio
     }
     /* --FIM-- PERMITE, OU NAO, O ACESSO DO USUARIO */
     
+    let filtros = {
+        mun: '',
+        uf :''
+    }
+    
+    
+
     function carregaController(){
     if (permitido) {
     
     $scope.municipios = [];
     
     var carregarMunicipios = function (){
-        //$http.get("http://172.25.117.3:3000/read/municipios").success(function(data, status){
             $http.get("/read/municipios").success(function(data, status){
-            //$scope.municipios = data;
             $scope.numPerPage = 50;
             $scope.noOfPages = Math.ceil(data.length/$scope.numPerPage);
             $scope.currentPage = 1;
@@ -41,29 +46,51 @@ angular.module('cidadesdigitais').controller('visuMunicipiosController', functio
             $scope.$watch('currentPage',$scope.setPage);
              $('#divLoading').hide();
         });
-    };
-  
-    var filtraMunicipio = function(buscado){
-        /* var endereco = "/read/municipios/:";
-        var end = endereco.concat(buscado);  */
-        $http.get("/read/municipios/").success(function(data, status){
-            $scope.municipios = data.filter((municipio) => {
-                return municipio.nome_municipio == buscado;
-            });
-            console.log(data.find((municipio) => {
-                return municipio.nome_municipio == buscado;
-            }));
-            $scope.numPerPage = 50;
-            $scope.noOfPages = Math.ceil(data.length/$scope.numPerPage);
-            $scope.currentPage = 1;
+    }; 
 
-            $scope.setPage = function(){
+    carregarMunicipios();
+
+    $scope.filtroMunicipio = function(filtros){
+        
+        if(!filtros.mun && !filtros.uf){
+            $http.get("/read/municipios").success(function(data, status){
+                $scope.numPerPage = 50;
+                $scope.noOfPages = Math.ceil(data.length/$scope.numPerPage);
+                $scope.currentPage = 1;
+    
+                $scope.setPage = function(){
+                    $scope.municipios = data.slice(($scope.currentPage-1)*$scope.numPerPage,(($scope.currentPage-1)*$scope.numPerPage)+$scope.numPerPage);
+                };
+                $scope.$watch('currentPage',$scope.setPage);
+                 $('#divLoading').hide();
+            });
+        }
+         else{
+             let valida;
+             
+             $http.get("/read/municipios/").success(function(data, status){
+             let municipio = data.filter(municipios =>{
+                 valida = true;
+                 if(filtros.mun && !municipios.nome_municipio.toString().toLowerCase().includes(filtros.mun.toLowerCase())){valida = false;}
+                 if(filtros.uf && !municipios.uf.toString().toLowerCase().includes(filtros.uf.toLowerCase())){valida = false;}
+
+                 return valida;
+             });
+
+             $scope.municipios = municipio;
+             $scope.numPerPage = 50;
+             $scope.noOfPages = Math.ceil($scope.municipios.length/$scope.numPerPage);
+             $scope.currentPage = 1;
+
+             $scope.setPage = function(){
                 $scope.municipios = $scope.municipios.slice(($scope.currentPage-1)*$scope.numPerPage,(($scope.currentPage-1)*$scope.numPerPage)+$scope.numPerPage);
-            };
-            $scope.$watch('currentPage',$scope.setPage);
+             };
+             $scope.$watch('currentPage',$scope.setPage);
              $('#divLoading').hide();
-        });
-    };
+             });
+         }
+    }
+  
     //    Metodo para retorna o array de estado armazenado no servidor
       $http.get('read/cd/estado')
         .success(function(estado){
@@ -72,21 +99,6 @@ angular.module('cidadesdigitais').controller('visuMunicipiosController', functio
         .error(function(error){
             console.log(error);
         });
-    $scope.filtroMun = function(){
-        var buscado = $scope.busca;
-        if($scope.busca == ""){
-            carregarMunicipios(); 
-        }else{
-            filtraMunicipio(buscado);
-        }
-    }
-    if($scope.busca){
-        filtraMunicipio();
-    }else{
-        carregarMunicipios(); 
-    
-    }
-    
     }
     else {
         var msg = "<strong>Aviso</strong><br><p>Você não tem a permissão necessária para acessar essa página.</p>";
